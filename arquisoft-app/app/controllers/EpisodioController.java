6b98b983f4aa28fb85e9dd9870b8dabd852d36bc
olpino521 <ol.pino521@uniandes.edu.co>
2015-02-18 11:30:51 -0500

    completo tres clases

--- arquisoft-app/app/controllers/EpisodioController.java
+++ arquisoft-app/app/controllers/EpisodioController.java
@@ -8,22 +8,37 @@ public class EpisodioController extends Controller
 
 	public static Result createEpisodio()
 	{
+//		Paciente newUser = Json.fromJson(request().body().asJson(), Paciente.class);
 		return ok(index.render("createEpisodio"));
+		
 	}
-	
+		
+	public static Result getEpisodios()
+	{
+//	    List<Paciente> users = Database.getUsers();
+//	    return ok(Json.toJson(users));
+	    return ok(index.render("getEpisodio"));
+	}
+
 	public static Result getEpisodio(Long id)
 	{
-		return ok(index.render("getEpisodio"));
+//	    User user = Database.getUser(id);
+//	    return user == null ? notFound() : ok(Json.toJson(user));
+	    return ok(index.render("getEpisodio"));
 	}
-	
+
 	public static Result updateEpisodio(Long id)
 	{
-		return ok(index.render("undateEpisodio"));
+//	    User user = Json.fromJson(request().body().asJson(), User.class);
+//	    User updated = Database.updateUser(id, user);
+//	    return ok(Json.toJson(updated));
+		return ok(index.render("updateEpisodio"));
 	}
-	
+
 	public static Result deleteEpisodio(Long id)
 	{
-		return noContent();
+//	    Database.deleteUser(id);
+	    return noContent(); // http://stackoverflow.com/a/2342589/1415732
 	}
 	
 }
