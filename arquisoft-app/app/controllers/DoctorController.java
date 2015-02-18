28af9b76420c8bee26629b124e075d0752aa4a45
nicofrost93 <nicofrost93@hotmail.com>
2015-02-18 10:42:21 -0500

    doctor commit

--- arquisoft-app/app/controllers/DoctorController.java
+++ arquisoft-app/app/controllers/DoctorController.java
@@ -1,9 +1,13 @@
 package controllers;
 
+import java.util.List;
 
+import models.Paciente;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+
 
 
 public class DoctorController extends Controller 
@@ -47,5 +51,39 @@ public class DoctorController extends Controller
     //---------------------------------------||
     //
 
+    public static Result createPaciente()
+	{
+//		Paciente newUser = Json.fromJson(request().body().asJson(), Paciente.class);
+		return ok(index.render("createPacientes"));
+		
+	}
+		
+	public static Result getDoctores()
+	{
+//	    List<Paciente> users = Database.getUsers();
+//	    return ok(Json.toJson(users));
+	    return ok(index.render("getDoctores"));
+	}
+
+	public static Result getDoctor(Long id)
+	{
+//	    User user = Database.getUser(id);
+//	    return user == null ? notFound() : ok(Json.toJson(user));
+	    return ok(index.render("getDoctor"));
+	}
+
+	public static Result updateDcotor(Long id)
+	{
+//	    User user = Json.fromJson(request().body().asJson(), User.class);
+//	    User updated = Database.updateUser(id, user);
+//	    return ok(Json.toJson(updated));
+		return ok(index.render("updateDoctor"));
+	}
+
+	public static Result deleteDoctor(Long id)
+	{
+//	    Database.deleteUser(id);
+		return ok(index.render("deleteDoctor")); // http://stackoverflow.com/a/2342589/1415732
+	}
 
 }
